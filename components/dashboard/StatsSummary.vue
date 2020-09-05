38fd1d6c1d3b1e61e50fb043f1f9971f87b519ca
rye gaspar <gaspar.ryan@outlook.com>
2020-09-04 21:57:35 -0500

    dashboard - summary, show negative values properly

--- components/dashboard/StatsSummary.vue
+++ components/dashboard/StatsSummary.vue
@@ -14,8 +14,9 @@
 					<div class="flex-1 text-right md:text-center">
 						<h5 class="font-bold uppercase text-gray-400">Total Revenue</h5>
 						<h3 class="font-bold text-3xl"
-							:class="[summaryTotal >= 0 ? 'text-gray-600' : 'text-gray-800']"
+							:class="[summaryTotal >= 0 ? 'text-gray-600' : 'text-red-800']"
 						>
+							{{ summaryTotal < 0 ? '-' : '' }}
 							&#8369;
 							<i-count-up :endVal="summaryTotal | number_format"
 										:options="options"
@@ -40,8 +41,9 @@
 					<div class="flex-1 text-right md:text-center">
 						<h5 class="font-bold uppercase text-gray-400">Income</h5>
 						<h3 class="font-bold text-3xl"
-							:class="[summaryIncome >= 0 ? 'text-gray-600' : 'text-gray-800']"
+							:class="[summaryIncome >= 0 ? 'text-gray-600' : 'text-red-800']"
 						>
+							{{ summaryIncome < 0 ? '-' : '' }}
 							&#8369;
 							<i-count-up :endVal="summaryIncome | number_format"
 										:options="options"
@@ -68,6 +70,7 @@
 						<h3 class="font-bold text-3xl"
 							:class="[summaryExpense >= 0 ? 'text-gray-600' : 'text-gray-800']"
 						>
+							{{ summaryExpense < 0 ? '-' : '' }}
 							&#8369;
 							<i-count-up :endVal="summaryExpense | number_format"
 										:options="options"
