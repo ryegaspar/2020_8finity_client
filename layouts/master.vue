e0c8f2ac176397480294ec2e610d30675d1eae4b
rye gaspar <gaspar.ryan@outlook.com>
2021-09-01 17:37:49 -0500

    remove keep-alive as it does not updates categories and accounts in the transaction modal

--- layouts/master.vue
+++ layouts/master.vue
@@ -6,7 +6,7 @@
 				<div class="flex flex-1 relative bg-black overflow-y-auto focus:outline-none pt-16 min-h-screen">
 					<side-bar/>
 					<main class="flex-1 md:pr-4 md:pl-10 w-full overflow-auto bg-black">
-						<nuxt keep-alive/>
+						<nuxt />
 					</main>
 				</div>
 			</div>
