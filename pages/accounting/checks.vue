38efca417637b46ced4bcc4661ae8c7d43bb016b
rye gaspar <gaspar.ryan@outlook.com>
2021-09-19 09:36:34 -0500

    add button to refresh tables on pages with tables

--- pages/accounting/checks.vue
+++ pages/accounting/checks.vue
@@ -1,15 +1,27 @@
 <template>
 	<div class="px-4 md:px-0 mb-16 mt-4 text-gray-400 leading-normal">
 		<div class="w-full px-3 py-1 sm:py-3">
-			<button
-				class="relative bg-gray-900 border border-gray-600 text-gray-600 py-2 px-3 rounded-lg items-center flex justify-between hover:border-blue-600 hover:text-blue-600 focus:outline-none"
-				@click.prevent="showModal({})"
-			>
-				<font-awesome-layers class="fa-fw mr-1">
-					<font-awesome-icon icon="plus-circle"/>
-				</font-awesome-layers>
-				New
-			</button>
+			<div class="flex justify-between">
+				<button
+					class="bg-gray-900 border border-gray-600 text-gray-600 py-2 px-3 rounded-lg items-center flex justify-between hover:border-blue-600 hover:text-blue-600 focus:outline-none text-sm md:text-base"
+					@click.prevent="showModal({})"
+				>
+					<font-awesome-layers class="fa-fw mr-1">
+						<font-awesome-icon icon="plus-circle"/>
+					</font-awesome-layers>
+					New
+				</button>
+
+				<button
+					class="bg-gray-900 border border-gray-600 text-gray-600 py-2 px-3 rounded-lg items-center flex justify-between hover:border-green-600 hover:text-green-600 focus:outline-none text-sm md:text-base"
+					@click.prevent="$refs.vuetable.refresh()"
+				>
+					<font-awesome-layers class="fa-fw mr-1">
+						<font-awesome-icon icon="sync"/>
+					</font-awesome-layers>
+					Refresh
+				</button>
+			</div>
 
 			<div class="flex flex-col">
 				<div class="mt-8 shadow overflow-hidden">
