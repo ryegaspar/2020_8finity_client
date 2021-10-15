6c954385372786370dbf772d98fead496e24d0e8
rye gaspar <gaspar.ryan@outlook.com>
2021-10-15 12:12:49 -0500

    add logout

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -14,44 +14,46 @@
 			<div class="flex-1 flex">
 			</div>
 			<div class="ml-4 flex items-center md:ml-6">
-				<notification />
+				<notification/>
 				<!-- Profile dropdown -->
 				<div class="ml-3 relative">
 					<div>
 						<button
 							class="max-w-xs flex items-center text-sm rounded-full focus:outline-none focus:shadow-outline"
-							id="user-menu" aria-label="User menu" aria-haspopup="true">
+							id="user-menu"
+							aria-label="User menu"
+							aria-haspopup="true"
+							@click="showUserMenu"
+							v-click-outside="hide"
+						>
 							<img class="h-8 w-8 rounded-full"
-								 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+								 :src="$auth.user.gravatar"
 								 alt="">
 						</button>
 					</div>
-					<!--
-					  Profile dropdown panel, show/hide based on dropdown state.
-
-					  Entering: "transition ease-out duration-100"
-						From: "transform opacity-0 scale-95"
-						To: "transform opacity-100 scale-100"
-					  Leaving: "transition ease-in duration-75"
-						From: "transform opacity-100 scale-100"
-						To: "transform opacity-0 scale-95"
-					-->
-					<div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg hidden">
-						<div class="py-1 rounded-md bg-white shadow-xs" role="menu"
-							 aria-orientation="vertical" aria-labelledby="user-menu">
-							<a href="#"
-							   class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
-							   role="menuitem">Your Profile</a>
-
-							<a href="#"
-							   class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
-							   role="menuitem">Settings</a>
-
-							<a href="#"
-							   class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
-							   role="menuitem">Sign out</a>
+					<transition
+						enter-active-class="transition ease-out duration-100"
+						enter-class="transform opacity-0 scale-95"
+						enter-to-class="transform opacity-100 scale-100"
+						leave-active-class="transition ease-in duration-75"
+						leave-class="transform opacity-100 scale-100"
+						leave-to-class="transform opacity-0 scale-95"
+					>
+						<div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg"
+							 v-if="userMenu"
+						>
+							<div class="py-1 rounded-md bg-gray-900 shadow-xs border border-gray-500" role="menu"
+								 aria-orientation="vertical" aria-labelledby="user-menu">
+								<a href="#"
+								   class="block px-4 py-2 text-sm text-gray-400 hover:bg-gray-200 transition ease-in-out duration-150"
+								   role="menuitem"
+								   @click.prevent="logout"
+								>
+									Sign out
+								</a>
+							</div>
 						</div>
-					</div>
+					</transition>
 				</div>
 			</div>
 		</div>
@@ -60,9 +62,33 @@
 
 <script>
 export default {
+	data() {
+		return {
+			userMenu: false,
+		}
+	},
+
+	mounted() {
+		console.log(this.$auth.user.gravatar)
+	},
+
 	methods: {
 		toggleMobileMenu() {
 			this.$root.$emit('toggle_menu')
+		},
+
+		showUserMenu() {
+			this.userMenu = !this.userMenu
+		},
+
+		hide() {
+			this.userMenu = false
+		},
+
+		async logout() {
+			await this.$auth.logout()
+
+			await this.$router.push('/login')
 		}
 	}
 }
